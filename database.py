d2717735c3a1a41cc53fbfe101ba93265eb36a29
kulkarnitejas02 <kukarnitejas002@gmail.com>
2026-04-06 22:56:38 +0530

    db code uodate

--- database.py
+++ database.py
@@ -8,11 +8,11 @@ DATABASE_URL = os.getenv("POSTGRES_URL")
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # DATABASE_URL = "sqlite:///./users.db"
 
-# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-# Base = declarative_base()
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
