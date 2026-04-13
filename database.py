0e00254b821f364a621c5ba78dd18f6d320915be
kulkarnitejas02 <kukarnitejas002@gmail.com>
2026-04-13 20:12:58 +0530

    db modified for postgress sql versal

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Vercel automatically provides POSTGRES_URL
-# Note: SQLAlchemy requires 'postgresql://' instead of 'postgres://' 
+# # Note: SQLAlchemy requires 'postgresql://' instead of 'postgres://' 
 DATABASE_URL = os.getenv("POSTGRES_URL")
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
@@ -13,6 +18,6 @@ if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
 
 # DATABASE_URL = "sqlite:///./users.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
